a9fe75ee66775be15f5fea58d8da12ce2d70150d
bayan <bayana.mhmd99@gmail.com>
2020-01-26 05:23:07 +0200

    empty

---
+++ categories.js
@@ -0,0 +1,21 @@
+'use strict';
+
+'use strict';
+
+const categoriesSchema = require('./categories-schema.js')
+
+class Categories {
+    constructor() {
+    }
+
+    get() { }
+
+    create() { }
+
+    update() { }
+
+    delete() { }
+
+}
+
+module.exports = Categories
